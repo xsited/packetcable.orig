3b2720a1cb8cfe1dd5ea9e7b146845a2d5e7d820
Thomas Kee <xsited@yahoo.com>
2013-12-01 22:35:08 -0800

    Bookmark some thoughs on information model via yang

--- src/main/java/org/pcmm/objects/PCMMIDHolder.java
+++ src/main/java/org/pcmm/objects/PCMMIDHolder.java
@@ -1,59 +1,62 @@
+/**
+ @header@
+ */
 package org.pcmm.objects;
 
 /**
  * this class holds and maps flow ID to PCMM gate ID and Transaction ID
- * 
+ *
  * @author rhadjamor@gmail.com
- * 
+ *
  */
 public class PCMMIDHolder extends PCMMResource {
 
-	/**
-	 * flow id.
-	 */
-	private int flowID;
-	/**
-	 * gate id.
-	 */
-	private int gateID;
-	/**
-	 * transaction id.
-	 */
-	private short transactionID;
+    /**
+     * flow id.
+     */
+    private int flowID;
+    /**
+     * gate id.
+     */
+    private int gateID;
+    /**
+     * transaction id.
+     */
+    private short transactionID;
 
-	public PCMMIDHolder(int flowID, int gateID, short transactionID) {
-		this.flowID = flowID;
-		this.gateID = gateID;
-		this.transactionID = transactionID;
+    public PCMMIDHolder(int flowID, int gateID, short transactionID) {
+        this.flowID = flowID;
+        this.gateID = gateID;
+        this.transactionID = transactionID;
 
-	}
+    }
 
-	public PCMMIDHolder() {
+    public PCMMIDHolder() {
 
-	}
+    }
 
-	public int getFlowID() {
-		return flowID;
-	}
+    public int getFlowID() {
+        return flowID;
+    }
 
-	public void setFlowID(int flowID) {
-		this.flowID = flowID;
-	}
+    public void setFlowID(int flowID) {
+        this.flowID = flowID;
+    }
 
-	public int getGateID() {
-		return gateID;
-	}
+    public int getGateID() {
+        return gateID;
+    }
 
-	public void setGateID(int gateID) {
-		this.gateID = gateID;
-	}
+    public void setGateID(int gateID) {
+        this.gateID = gateID;
+    }
 
-	public short getTransactionID() {
-		return transactionID;
-	}
+    public short getTransactionID() {
+        return transactionID;
+    }
 
-	public void setTransactionID(short transactionID) {
-		this.transactionID = transactionID;
-	}
+    public void setTransactionID(short transactionID) {
+        this.transactionID = transactionID;
+    }
 
 }
--- src/main/java/org/pcmm/objects/PCMMResourceSet.java
+++ src/main/java/org/pcmm/objects/PCMMResourceSet.java
@@ -1,3 +1,6 @@
+/**
+ @header@
+ */
 package org.pcmm.objects;
 
 import java.util.HashMap;
@@ -5,50 +8,50 @@ import java.util.Map;
 
 /**
  * This stores and handles the PCMM resources.
- * 
+ *
  * @author rhadjamor@gmail.com
- * 
+ *
  */
 public class PCMMResourceSet {
 
-	private Map<Object, PCMMResourcesMapper<?, ?>> mapper;
+    private Map<Object, PCMMResourcesMapper<?, ?>> mapper;
 
-	private static PCMMResourceSet instance;
+    private static PCMMResourceSet instance;
 
-	private PCMMResourceSet() {
-		mapper = new HashMap<Object, PCMMResourcesMapper<?, ?>>();
-	}
+    private PCMMResourceSet() {
+        mapper = new HashMap<Object, PCMMResourcesMapper<?, ?>>();
+    }
 
-	public static PCMMResourceSet getInstance() {
-		if (instance == null)
-			instance = new PCMMResourceSet();
-		return instance;
-	}
+    public static PCMMResourceSet getInstance() {
+        if (instance == null)
+            instance = new PCMMResourceSet();
+        return instance;
+    }
 
-	/**
-	 * adds a new mapping
-	 * 
-	 * @param key
-	 *            to be used for identifying mapped structure
-	 * @return resource mapper
-	 */
-	@SuppressWarnings("unchecked")
-	public <M, T extends PCMMResource> PCMMResourcesMapper<M, T> getMappedResources(
-			Object key) {
-		return (PCMMResourcesMapper<M, T>) mapper.get(key);
-	}
+    /**
+     * adds a new mapping
+     *
+     * @param key
+     *            to be used for identifying mapped structure
+     * @return resource mapper
+     */
+    @SuppressWarnings("unchecked")
+    public <M, T extends PCMMResource> PCMMResourcesMapper<M, T> getMappedResources(
+        Object key) {
+        return (PCMMResourcesMapper<M, T>) mapper.get(key);
+    }
 
-	public <M, T extends PCMMResource> void mapResources(Object key,
-			PCMMResourcesMapper<M, T> resources) {
-		mapper.put(key, resources);
-	}
+    public <M, T extends PCMMResource> void mapResources(Object key,
+            PCMMResourcesMapper<M, T> resources) {
+        mapper.put(key, resources);
+    }
 
-	public void removeMapping(Object key) {
-		mapper.remove(key);
-	}
+    public void removeMapping(Object key) {
+        mapper.remove(key);
+    }
 
-	public void removeAllMappings() {
-		mapper.clear();
-	}
+    public void removeAllMappings() {
+        mapper.clear();
+    }
 
 }
